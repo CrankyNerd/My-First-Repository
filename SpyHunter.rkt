0bf15943cc0b344d906b078f8b45ac62b43ded36
CrankyNerd <gdj@CrankyNerd.local>
2015-09-26 22:03:35 -0700

    Made spy, friendly-car, and truck images
    made images. need to base pos. of images off of W and H constants

--- SpyHunter.rkt
+++ SpyHunter.rkt
@@ -61,6 +61,44 @@
 (define W 600)
 (define H 800)
 
+(define straight-road (beside (rectangle (* 1/6 W) (* 3 800) 'solid 'green)
+                              (rectangle (* 2/3 W) (* 3 800) 'solid 'black)
+                              (rectangle (* 1/6 W) (* 3 800) 'solid 'green)))
+;; BG for now. need to make curvy road images and place them above straight-road
+(define BG straight-road)
+
+;; spy car -- BASE POS. OF STRIPES + ROOF OFF W AND H
+(define spy-body (rectangle (* 1/20 W) (* 1/15 H) 'solid 'burlywood))
+(define spy-roof (rectangle (* 1/30 W) (* 1/60 H) 'solid 'moccasin))
+(define spy-stripes (beside(rectangle (* 1/80 W) (* 1/45 H) 'solid 'blue)
+                           (empty-scene (* 1/80 W) 0)
+                           (rectangle (* 1/80 W) (* 1/45 H) 'solid 'blue)))
+(define spy (place-image spy-stripes 15 15
+                         (place-image spy-roof 15 35 spy-body)))
+
+;; friendly car
+; uses the same code as spy, but with different colors.
+(define frnd (place-image (beside(rectangle (* 1/80 W) (* 1/45 H)
+                                            'solid 'black)
+                                 (empty-scene (* 1/80 W) 0)
+                                 (rectangle (* 1/80 W) (* 1/45 H)
+                                            'solid 'black))
+                          15 15
+                          (place-image
+                           (rectangle (* 1/30 W) (* 1/60 H) 'solid 'firebrick)
+                           15 35
+                           (rectangle (* 1/20 W) (* 1/15 H) 'solid 'darkred))
+                          ))
+;; helper truck
+(define trailer (rectangle (* 1/20 W) (* 1/12 H) 'solid 'darkred))
+(define connector (rectangle (* 1/100 W) (* 1/180 H) 'solid 'firebrick))
+(define nose (above (rectangle (* 1/25 W) (* 1/130 H) 'solid 'red)
+                    (rectangle (* 1/20 W) (* 1/25 H) 'solid 'darkred)))
+(define truck (above nose connector trailer))
+
+
+
+
 
 
 
